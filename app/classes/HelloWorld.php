3bc28e0b85b4962bcc7265151ff7969dc99949cb
fahim2200 <fahimrahman223@gmail.com>
2022-01-27 13:10:25 +0600

    php day1

---
+++ app/classes/HelloWorld.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace App\classes;
+
+
+class HelloWorld
+{
+    public $name = 'BITM';
+    public function index()
+
+    {
+//        *3 major rules for variables
+//           start with $,
+//            a-z,A-Z,0-9,
+//        *no number in first
+        $name = 1000;
+        echo $name;
+    }
+
+
+
+
+
+    
+}
